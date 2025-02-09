0f1ec6c60918c3a96d32566f4689d37f8d3a5063
BiaoWang <970122628@qq.com>
2025-02-09 19:25:52 +0800

    将人体识别模型换成yolo11，解决了mediapipe库的打包错误

--- src/manager/take_photo/take_a_photo.py
+++ src/manager/take_photo/take_a_photo.py
@@ -7,7 +7,55 @@ from .get_best_photo import capture_best_photo
 import cv2
 import sys
 from ..get_location import save_image_with_gps
+from ultralytics import YOLO
 import mediapipe as mp
+
+
+def detect_person_media_pipe(frame):
+    # 打包成exe会出现问题，暂时不用
+    # 初始化MediaPipe人体检测
+    mp_pose = mp.solutions.pose
+    # 加载预训练的YOLOv11模型
+    pose = mp_pose.Pose(static_image_mode=False,        # 静态图像模式，只检测一次
+                        min_detection_confidence=0.1,  # 检测置信度
+                        model_complexity=1,            # 简化模型，减少反馈警告
+                        enable_segmentation=False)     # 如果不需要分割功能，禁用它
+
+    # 转换为RGB格式
+    # 计算处理的时间
+    image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    person_count = 0
+    if pose.process(image_rgb).pose_landmarks:
+        person_count = 1
+    return person_count
+
+
+def detect_person_yolo11(image):
+    """
+    检测图像中是否有人。
+
+    参数：
+        image (str 或 numpy.ndarray): 图像的文件路径或图像数组。
+
+    返回：
+        bool: 如果检测到人，返回True；否则返回False。
+    """
+    # 加载预训练的YOLOv11模型
+    model = YOLO("yolo11n.pt")
+
+    # 对图像进行预测, verbose=False表示进行推理时禁用控制台输出
+    results = model.predict(source=image, verbose=False)
+
+    # 初始化计数器
+    person_count = 0
+
+    # 遍历检测结果
+    for result in results:
+        for box in result.boxes:
+            cls = box.cls
+            if cls == 0:  # 在COCO数据集中，类别0对应于'person'
+                person_count += 1
+    return person_count
 
 
 def take_photo(cam, latitude, longitude):
@@ -18,19 +66,15 @@ def take_photo(cam, latitude, longitude):
     # 直接拍照
     frame = capture_best_photo(cam)
     t1 = cv2.getTickCount()
-    # 初始化MediaPipe人体检测
-    mp_pose = mp.solutions.pose
-    pose = mp_pose.Pose(static_image_mode=True, min_detection_confidence=0.1)
-    # 转换为RGB格式
-    # 计算处理的时间
-    image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    results = pose.process(image_rgb)
+    # results = detect_person_media_pipe(frame)
+    results = detect_person_yolo11(frame)
     t2 = cv2.getTickCount()
     time = (t2 - t1) / cv2.getTickFrequency()
     fps = 1.0 / time
 
-    if results.pose_landmarks:
-        print(f"Time {datetime.now().strftime('%Y-%m-%d %H:%M:%S')} Detected a person, photo captured! Time: {time}, FPS: {fps}")
+    if results:
+        # 输出检测到几个人
+        print(f"Time {datetime.now().strftime('%Y-%m-%d %H:%M:%S')} Detected {results} person(s) in the photo Time: {time}, FPS: {fps}")
 
         print(f"Time {datetime.now().strftime('%Y-%m-%d %H:%M:%S')} Saving photo")
         # 生成当前时间的时间戳
